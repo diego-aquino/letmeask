3e72c37f5e75214336b95a3e8121cbcf0b8775c2
Diego Aquino <diego.cruz001@gmail.com>
2021-06-20 21:52:15 -0300

    Set up firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '~/styles/global';
 import theme from '~/styles/theme';
 
+import '~/services/firebase';
+
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
