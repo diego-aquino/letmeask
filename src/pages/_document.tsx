b02a42abf9b4ef25e4829c8fb1a5c7fd561b21d6
Diego Aquino <diego.cruz001@gmail.com>
2021-06-21 22:02:58 -0300

    global

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,13 @@ class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto:wght@400;500&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
