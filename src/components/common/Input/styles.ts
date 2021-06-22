7dbf3029e25386fcf075381e0746d3319f44022c
Diego Aquino <diego.cruz001@gmail.com>
2021-06-22 16:41:29 -0300

    Implement label/placeholder input interchange

--- src/components/common/Input/styles.ts
+++ src/components/common/Input/styles.ts
@@ -1,19 +1,67 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.input`
-  ${({ theme }) => css`
-    height: 5rem;
-    border-radius: 0.8rem;
-    padding: 0 1.6rem;
-    border: 1px solid ${theme.colors.gray.medium};
-    outline: none;
+interface ContainerProps {
+  keepLabelOutside?: boolean;
+}
 
-    background-color: ${theme.colors.white.details};
-    transition: border-color 0.15s, box-shadow 0.15s;
+export const Container = styled.div<ContainerProps>`
+  ${({ theme, keepLabelOutside }) => css`
+    position: relative;
 
-    :focus {
-      border-color: ${theme.colors.purple};
-      box-shadow: 0px 0px 0px 1px ${theme.colors.purple};
+    label {
+      position: absolute;
+      top: 50%;
+      left: 1.6rem;
+
+      color: ${theme.colors.black};
+
+      transform: translateY(-50%);
+      opacity: 0.7;
+      transition: top 0.15s, left 0.15s, transform 0.15s, font-weight 0.15s,
+        color 0.15s, opacity 0.15s;
+      cursor: text;
+
+      ${keepLabelOutside &&
+      css`
+        top: -2.6rem;
+        left: 0;
+
+        transform: translateY(0%);
+        opacity: 1;
+      `}
+    }
+
+    input {
+      height: 5rem;
+      border-radius: 0.8rem;
+      padding: 0 1.6rem;
+      border: 1px solid ${theme.colors.gray.medium};
+      outline: none;
+
+      background-color: ${theme.colors.white.details};
+      transition: border-color 0.15s, box-shadow 0.15s;
+
+      :focus {
+        border-color: ${theme.colors.purple};
+        box-shadow: 0px 0px 0px 1px ${theme.colors.purple};
+      }
+
+      ::placeholder {
+        opacity: 0;
+      }
+    }
+
+    :focus-within {
+      label {
+        top: -2.6rem;
+        left: 0;
+
+        font-weight: 500;
+        color: ${theme.colors.hover.purple};
+        transform: translateY(0%);
+
+        opacity: 1;
+      }
     }
   `}
 `;
